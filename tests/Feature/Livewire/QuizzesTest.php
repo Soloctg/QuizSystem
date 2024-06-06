5fa77b2a52bfd78b9b18f9731521a05f07dbe1fd
solomon-ctg <solomon.iroegbu@gmail.com>
2024-06-06 19:56:34 +0100

    feat: make Quizzes Test

---
+++ tests/Feature/Livewire/QuizzesTest.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Models\User;
+use App\Models\Quiz;
+use Illuminate\Foundation\Testing\TestCase;
+use Livewire\Livewire;
+use App\Models\Question;
+use App\Livewire\Quiz\QuizForm;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class QuizzesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testAdminCanCreateQuiz()
+    {
+        $this->actingAs(User::factory()->admin()->create());
+
+        Livewire::test(QuizForm::class)
+            ->set('title', 'quiz title')
+            ->call('save')
+            ->assertHasNoErrors(['title', 'slug', 'description', 'published', 'public', 'questions'])
+            ->assertRedirect(route('quizzes'));
+
+        $this->assertDatabaseHas('quizzes', [
+            'title' => 'quiz title',
+        ]);
+    }
+
+    public function testTitleIsRequired()
+    {
+        $this->actingAs(User::factory()->admin()->create());
+
+        Livewire::test(QuizForm::class)
+            ->set('title', '')
+            ->call('save')
+            ->assertHasErrors(['title' => 'required']);
+    }
+
+    public function testAdminCanEditQuiz()
+    {
+        $this->actingAs(User::factory()->admin()->create());
+
+        $quiz = Quiz::factory()
+            ->has(Question::factory())
+            ->create();
+
+        Livewire::test(QuizForm::class, [$quiz])
+            ->set('title', 'new quiz')
+            ->set('published', true)
+            ->call('save')
+            ->assertSet('published', true)
+            ->assertHasNoErrors(['title', 'slug', 'description', 'published', 'public', 'questions']);
+
+        $this->assertDatabaseHas('quizzes', [
+            'title' => 'new quiz',
+            'published' => true,
+        ]);
+    }
+}
